c378f5174d6d6280844a78894dbf9bac0b02dfd3
andrea <benedetti.a93@gmail.com>
2025-01-23 14:36:35 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   output: 'export',
-  basePath: isProd ? '/syllabus' : '',
+  basePath: '/syllabus',
   trailingSlash: true,
-}
+};
