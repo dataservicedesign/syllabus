cebe62b64e95c977ec34e47d47dcb1574ac1727c
andrea <benedetti.a93@gmail.com>
2025-01-23 15:22:05 +0100

    meta

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from "next/head";
-import Bib from "../components/bib";
 import Navigation from "../components/nav";
-import Link from "next/link";
 import { getSortedPostsData } from "../lib/projectPost";
-import Teaching from "../components/Teaching";
 import Footer from "../components/footer";
 import Image from "next/image";
 
@@ -21,20 +18,20 @@ export default function Home({ allPostsData }) {
     <>
     <div className="container">
     <Head>
-    <title>bndndr, Andrea Benedetti</title>
-    <meta name="title" content="bndndr, Andrea Benedetti" key="title" />
-    <meta name="description" content="Design portfolio of Andrea Benedetti, researcher and practitioner." key="description" />
+    <title>Data and Service Design - Politecnico Milano</title>
+    <meta name="title" content="Data and Service Design - Politecnico Milano" key="title" />
+    <meta name="description" content="A course on data practices and service design at Politecnico Milano School of Design, Master in Product Service System Design" key="description" />
     
     <meta property="og:type" content="website" key="og:type"/>
-    <meta property="og:url" content="https://www.bndndr.it/" key="og:url"/>
-    <meta property="og:title" content="bndndr, Andrea Benedetti" key="og:title"/>
-    <meta property="og:description" content="Design portfolio of Andrea Benedetti, researcher and practitioner." key="og:description"/>
+    <meta property="og:url" content="https://dataservicedesign.github.io/syllabus" key="og:url"/>
+    <meta property="og:title" content="Data and Service Design - Politecnico Milano" key="og:title"/>
+    <meta property="og:description" content="A course on data practices and service design at Politecnico Milano School of Design, Master in Product Service System Design" key="og:description"/>
     <meta property="og:image" content="/og-image.png" key="og:image"/>
     
     <meta property="twitter:card" content="summary_large_image" key="twitter:card"/>
-    <meta property="twitter:url" content="https://www.bndndr.it/" key="twitter:url"/>
-    <meta property="twitter:title" content="bndndr, Andrea Benedetti" key="twitter:title"/>
-    <meta property="twitter:description" content="Design portfolio of Andrea Benedetti, researcher and practitioner." key="twitter:description"/>
+    <meta property="twitter:url" content="https://dataservicedesign.github.io/syllabus" key="twitter:url"/>
+    <meta property="twitter:title" content="Data and Service Design - Politecnico Milano" key="twitter:title"/>
+    <meta property="twitter:description" content="A course on data practices and service design at Politecnico Milano School of Design, Master in Product Service System Design" key="twitter:description"/>
     <meta property="twitter:image" content="/og-image.png" key="twitter:image"/>
     
     </Head>
